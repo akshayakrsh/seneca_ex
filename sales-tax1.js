f509a51cbe7998878f0a34a702994516fe8dabdf
Lord <Akshaya_Kumar_Sharma@mckinsey.com>
2015-08-20 15:14:50 +0530

    Sales Tax 1 updated

--- sales-tax1.js
+++ sales-tax1.js
@@ -1,38 +1,47 @@
 var seneca = require('seneca')();
 
 seneca.add({
-    cmd:'salestax'
+    cmd: 'salestax'
   },
-  function( msg, respond ) {
-    var total = msg.net + (msg.net * 20/100)
-    respond( null, { answer: total } )
+  function (msg, respond) {
+    var rate = msg.hasOwnProperty('rate') ? msg.rate : 20;
+    var total = msg.net + (msg.net * rate / 100)
+    respond(null, {answer: total})
   });
+
 
 seneca.add({
-  cmd:'salestax',
-  country:'US'
-},
-  function( msg, respond ) {
-    var total = msg.net * ( 1 + 1/20 )
-    respond( null, { answer: total } )
+    cmd: 'salestax',
+    country: 'US'
+  },
+  function (msg, respond) {
+    seneca.act({
+        cmd: 'salestax',
+        net: msg.net,
+        rate: 50
+      },
+      function (err, result) {
+        respond(err, result)
+      })
+
   });
 
 
-seneca.act( {
-    cmd:'salestax',
+seneca.act({
+    cmd: 'salestax',
     net: 100
-},
-  function( err, result ) {
-    if( err ) return console.error( err )
-    console.log( result )
-  })
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
 
-seneca.act( {
-    cmd:'salestax',
+seneca.act({
+    cmd: 'salestax',
     net: 100,
-  country: 'US'
-},
-  function( err, result ) {
-    if( err ) return console.error( err )
-    console.log( result )
-  })
+    country: 'US'
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
