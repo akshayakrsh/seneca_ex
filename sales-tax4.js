9068ee7fe8a64810a8217b0272034d0cf4ae4030
Lord <Akshaya_Kumar_Sharma@mckinsey.com>
2015-08-20 16:24:49 +0530

    Sales Tax 4 ex

---
+++ sales-tax4.js
@@ -0,0 +1,136 @@
+var seneca = require('seneca')();
+
+function sales_tax_plugin() {
+  seneca.add({
+      cmd: 'salestax'
+    },
+    function (msg, respond) {
+      var rate = msg.hasOwnProperty('rate') ? msg.rate : 20;
+      var total = msg.net + (msg.net * rate / 100);
+      respond(null, {answer: total})
+    });
+
+  seneca.add({
+      cmd: 'salestax',
+      country: 'US'
+    },
+    function (msg, respond) {
+      seneca.act({
+          cmd: 'salestax',
+          net: msg.net,
+          rate: msg.hasOwnProperty('rate') ? msg.rate : 25
+        },
+        respond)
+    });
+
+  seneca.add({
+      cmd: 'salestax',
+      country: 'US',
+      state: 'NY'
+    },
+    function (msg, respond) {
+      seneca.act({
+          cmd: 'salestax',
+          country: msg.country,
+          net: msg.net,
+          rate: msg.hasOwnProperty('rate') ? msg.rate : 50
+        },
+        respond)
+    });
+
+  seneca.add({
+      cmd: 'salestax',
+      country: 'US',
+      state: 'NY',
+      city: 'NYC'
+    },
+    function (msg, respond) {
+      seneca.act({
+          cmd: 'salestax',
+          country: msg.country,
+          state: msg.state,
+          net: msg.net,
+          rate: 70
+        },
+        respond)
+    });
+
+  seneca.add({
+      cmd: 'salestax',
+      country: 'US',
+      state: 'NY',
+      county: 'Brooklyn'
+    },
+    function (msg, respond) {
+      if(msg.hasOwnProperty('clothing_net'))
+        msg.net -= msg.clothing_net;
+
+      seneca.act({
+          cmd: 'salestax',
+          country: msg.country,
+          state: msg.state,
+          net: msg.net,
+          rate: 70
+        },
+        respond)
+    });
+}
+
+seneca.use(sales_tax_plugin);
+
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US'
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US',
+    state: 'NY'
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US',
+    state: 'NY',
+    city: 'NYC'
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
+
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    clothing_net: 20,
+    country: 'US',
+    state: 'NY',
+    county: 'Brooklyn'
+  },
+  function (err, result) {
+    if (err) return console.error(err);
+    console.log(result)
+  });
